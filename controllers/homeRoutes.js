95b10623d9da60981fc71bf585a3a2dc02eb54a0
Amman Nega <ammannega@gmail.com>
2023-05-07 00:36:07 -0700

    added page to view each post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,6 +29,29 @@ router.get('/', async (req, res) => {
     }
 });
 
+// view blog post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+          ],
+        });
+    
+        const post = postData.get({ plain: true });
+    
+        res.render('post', {
+          ...post,
+          logged_in: req.session.logged_in
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
 // view login page
 router.get('/login', (req, res) => {
     // if the user is already logged in, redirect to homepage
