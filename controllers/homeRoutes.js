2bbf9466e8d38ae89a15851a19c2dc4682efa18a
Amman Nega <ammannega@gmail.com>
2023-05-07 20:21:46 -0700

    fixed bug where the incorrect comment creator displayed under said comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,7 +19,6 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({plain: true}));
 
         // render homepage with all blog posts
-        console.log("Posts: " + JSON.stringify(posts));
         res.render('homepage', {
             posts,
             logged_in: req.session.logged_in
@@ -27,6 +26,33 @@ router.get('/', async (req, res) => {
     } catch(err) {
         res.status(500).json(err);
     }
+});
+
+// view dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
+  // TODO: get all posts associated with current user
+  try {
+    // get all blog posts
+    const postData = await Post.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['username'], // don't want to include password in result
+            },
+        ],
+    });
+
+    // serialize data
+    const posts = postData.map((post) => post.get({plain: true}));
+
+    // render homepage with all blog posts
+    res.render('dashboard', {
+        posts,
+        logged_in: req.session.logged_in
+    });
+} catch(err) {
+    res.status(500).json(err);
+}
 });
 
 // view blog post
@@ -77,6 +103,7 @@ router.get('/post/:id/leave-comment', withAuth, async (req, res) => {
 
         res.render('add-comment', {
           ...post,
+          user_id: req.session.user_id,
           logged_in: req.session.logged_in
         });
       } catch (err) {
